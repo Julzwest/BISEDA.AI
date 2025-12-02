5c76af3216a21a2c32bebb17fe680cf85027da15
Julzwest <yxjgvsrv26@privaterelay.appleid.com>
2025-12-02 04:55:51 +0000

    Fix production build: deploy compiled assets

---
+++ assets/index-BlWQ4HHA.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DRCw6Mjo.js","assets/index-Becz53K4.js","assets/index-D-iyTyRL.css"])))=>i.map(i=>d[i]);
+import{r as i,_ as t}from"./index-Becz53K4.js";const p=i("SignInWithApple",{web:()=>t(()=>import("./web-DRCw6Mjo.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SignInWithAppleWeb)});export{p as SignInWithApple};
