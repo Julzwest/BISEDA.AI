26f7df20c6dcfdcf3281ec146afcd0bab3f92a52
Julzwest <yxjgvsrv26@privaterelay.appleid.com>
2025-12-02 05:08:41 +0000

    Fix build and deploy Evente Lokale

---
+++ assets/index-BdoII2Zq.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bw-RnOs0.js","assets/index-CSLNY2hm.js","assets/index-CqcGymAO.css"])))=>i.map(i=>d[i]);
+import{r as i,_ as t}from"./index-CSLNY2hm.js";const p=i("SignInWithApple",{web:()=>t(()=>import("./web-Bw-RnOs0.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SignInWithAppleWeb)});export{p as SignInWithApple};
